71d9ba27ac36c28fd03b48e10d415897ddb1063d
BruecknerR <mail@ruben-brueckner.de>
2014-05-01 11:38:47 +0200

    Einige Texte hinzugefügt.

--- BlackJack_Uebung/src/Spieltisch.java
+++ BlackJack_Uebung/src/Spieltisch.java
@@ -14,6 +14,8 @@ public class Spieltisch {
    */
   public void begruessung() {
   System.out.println("Willkommen bei BlackJack with Java!");
+  //p = new Kartenhand();
+  
   }
 
   /** 
@@ -21,13 +23,27 @@ public class Spieltisch {
    *  Zeigt Aktuelle Kartenhand an + Aktuellen Handwert
    *  Fenster, das Möglichkeit zur Auswahl gibt. Liefert true oder false zurück.
    */
+  //public integer spielerAuswertung = 5;
   public void willstDuKarte() {
+	  
+	  if (5 < zielWertHand) {
+		//getTextdarstellung Kartenhand Spieler
+		System.out.println("Möglich. TODO: Danach Fragen, ob er neue Karte will");
+	} else {
+System.out.println("Nicht möglich");
+	}
+	  
   }
 
   /** 
    *  Gibt auswertung Spiel aus und fragt nach neuem Spiel.
    */
   public void spielBeenden() {
+	  auswertungSpiel();
+	  System.out.println("Möchtest du nochmal?");
+	  //hier fragen mit Dialogbox
+	  //erstmal wird nur beendet
+	  System.exit(0);
   }
 
 }
