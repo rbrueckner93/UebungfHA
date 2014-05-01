2856335707b29538eecfaa75bcacf6400c7d3572
Nils <Nils@192.168.1.56>
2014-05-01 11:21:00 +0200

    Erstellung Grundgerüst

---
+++ BlackJack_Uebung/src/Spielkarte.java
@@ -0,0 +1,41 @@
+public class Spielkarte {
+
+  public String farbe;
+
+  public String bezeichnung;
+
+  /** 
+   *  Array mit erkaubten Farben
+   */
+  public null möglicheFarben array final;
+
+  /** 
+   *  Array mit erlaubten Bezeichnungen
+   */
+  private null möglicheBezeichnungen array final;
+
+  /** 
+   *  Gibt die farbe wieder
+   */
+  public getfarbe() {
+  }
+
+  /** 
+   *  Gibt die bezeichnung wieder
+   */
+  public void getBezeichnung() {
+  }
+
+  /** 
+   *  Gibt einen String aus Farbe+bezeichnung zurück
+   */
+  public void getTextdarstellung() {
+  }
+
+  /** 
+   *  Gibt true zurück, wenn Karte ein Ass ist
+   */
+  public void istAss() {
+  }
+
+}
