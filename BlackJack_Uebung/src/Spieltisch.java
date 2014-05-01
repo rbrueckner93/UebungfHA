a93ac35f26dcba84208c9c2bacec388a8e8ae7c7
rbrueckner93 <mail@ruben-brueckner.de>
2014-05-01 12:24:44 +0200

    Update Spieltisch.java
    instance spielerhand of Kartenhand
    Großbuchstaben zu Kleinbuchstaben korrigiert.

--- BlackJack_Uebung/src/Spieltisch.java
+++ BlackJack_Uebung/src/Spieltisch.java
@@ -3,27 +3,27 @@ public class Spieltisch {
 	public final Integer zielWertHand = 21;
 
 	/**
-	 * Wertet die Hände nach geltenden BlackJack Regeln aus. Auswertung nach
-	 * Anzahl der Asse. Günstigste Wahl der Wertigkeiten
+	 * Wertet die Hï¿½nde nach geltenden BlackJack Regeln aus. Auswertung nach
+	 * Anzahl der Asse. Gï¿½nstigste Wahl der Wertigkeiten
 	 */
 	public void auswertungSpiel() {
 	}
 
 	/**
-	 * Begrüßung des Spielers und erstellen der notwendigen
+	 * Begrï¿½ï¿½ung des Spielers und erstellen der notwendigen
 	 * Objekte(Stapel,Spielerhand,Dealerhand) automatisches Ziehen der ersten
 	 * Spielkarte
 	 */
 	public void begruessung() {
 		System.out.println("Willkommen bei BlackJack with Java!");
-		Kartenhand Spielerhand = new Kartenhand();
+		Kartenhand spielerhand = new Kartenhand();
 		System.out.println("Wir haben auch schon eine Kartenhand generiert.");
 	}
 
 	/**
 	 * auswertungSpiel() checkt, ob Spiel fortzusetzten ist Zeigt Aktuelle
-	 * Kartenhand an + Aktuellen Handwert Fenster, das Möglichkeit zur Auswahl
-	 * gibt. Liefert true oder false zurück.
+	 * Kartenhand an + Aktuellen Handwert Fenster, das Mï¿½glichkeit zur Auswahl
+	 * gibt. Liefert true oder false zurï¿½ck.
 	 */
 
 	public void willstDuKarte() {
@@ -32,9 +32,9 @@ public class Spieltisch {
 		if (spielerAuswertung < zielWertHand) {
 			// getTextdarstellung Kartenhand Spieler
 			System.out
-					.println("Möglich. TODO: Danach Fragen, ob er neue Karte will");
+					.println("Mï¿½glich. TODO: Danach Fragen, ob er neue Karte will");
 		} else {
-			System.out.println("Nicht möglich");
+			System.out.println("Nicht mï¿½glich");
 		}
 
 	}
@@ -44,7 +44,7 @@ public class Spieltisch {
 	 */
 	public void spielBeenden() {
 		auswertungSpiel();
-		System.out.println("Möchtest du nochmal?");
+		System.out.println("Mï¿½chtest du nochmal?");
 		// hier fragen mit Dialogbox
 		// erstmal wird nur beendet
 		System.exit(0);
