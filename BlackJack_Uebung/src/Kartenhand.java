95367e42f9fa5ef9eacd33ca85645eb6f3e5a864
Nils <Nils@>
2014-05-04 13:36:57 +0200

    Auswertmethode für OptimalenWert hinzugefügt

--- BlackJack_Uebung/src/Kartenhand.java
+++ BlackJack_Uebung/src/Kartenhand.java
@@ -5,6 +5,7 @@ public class Kartenhand {
 	 */
 	public Spielkarte[] spielHand;
 	public int kartenAufHand = 0;
+	public static final int blackJack = 21;
 
 	public Kartenhand() {
 		spielHand = new Spielkarte[(Spielkarte.erzeugeMoeglicheBezeichnungen().length * Spielkarte
@@ -36,11 +37,46 @@ public class Kartenhand {
 	 */
 	public String handAnzeigen() {
 		String Kartentext = null;
-		for (int laeufer = 0; laeufer <= kartenAufHand; laeufer++) {
+		for (int laeufer = 0; laeufer < kartenAufHand; laeufer++) {
 			Spielkarte Karte = spielHand[laeufer];
 			Kartentext = Kartentext + " " + Karte.getTextdarstellung();
-		} 
+		}
 		return Kartentext;
 	}
 
+	public int getOptimalenWert() {
+		int letzterWert = 0;
+		int ohneAssWert = 0;
+		int mitAssWert = 0;
+		int optimalWert = 0;
+		int anzahlAsse = 0;
+		for (int laeufer = 0; laeufer < kartenAufHand; laeufer++) {
+			mitAssWert += spielHand[laeufer].getIntWert();
+			if (spielHand[laeufer].istAss()) {
+				anzahlAsse++;
+			}
+		}
+		if (anzahlAsse != 0) {
+			ohneAssWert = mitAssWert - (anzahlAsse * 11);
+			int[] handWerte = new int[(kartenAufHand - 1)];
+			for (int i = 0; i <= anzahlAsse; i++) {
+				int asseMitelf = i;
+				int asseMiteins = anzahlAsse - i;
+				handWerte[i] = ohneAssWert + (asseMiteins * 1)
+						+ (asseMitelf * 11);
+			}
+			for (int auswerteWert : handWerte) {
+				if (auswerteWert <= 21 && auswerteWert > mitAssWert
+						&& auswerteWert > letzterWert) {
+					letzterWert = auswerteWert;
+					optimalWert = auswerteWert;
+				} else {
+					optimalWert = mitAssWert;
+				}
+			}
+		} else {
+			optimalWert = mitAssWert;
+		}
+		return optimalWert;
+	}
 }
